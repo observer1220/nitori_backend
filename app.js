446e66c1e4fe7b4cdb087dab77aa2551e56bbe00
observer1220 <observer1220@gmail.com>
2024-12-12 11:49:39 +0800

    更新CORS

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 // app.js
 const express = require("express");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const Product = require("./models/product");
 const sequelize = require("./config/database");
 
 const app = express();
 app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5173/",
+      "http://64.176.37.84",
+      "http://64.176.37.84/",
+    ], // 允許的前端域名
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 // 連接資料庫
 sequelize
