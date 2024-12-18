c402760c56847ba983934e1d632685adba973125
observer1220 <observer1220@gmail.com>
2024-12-18 16:42:40 +0800

    2024-12-18

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Step1: 更新原始碼至 Github／Gitlab
+git add .
+git commit -m "$(date +'%Y-%m-%d')"
+git push origin main
+
+# Step2: 登入 VPS 並執行更新及重啟服務
+ssh root@64.176.37.84 << EOF
+  cd nitori_furniture
+  git pull
+  docker compose up -d
+EOF
