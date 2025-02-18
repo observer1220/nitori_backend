bd47d6227a3ee44897635b3597f258130c16c5d3
ＫＥＮＴ <kent@Kent-Mac-mini.local>
2025-02-18 16:20:25 +0800

    2025-02-18

--- models/warehouse.js
+++ models/warehouse.js
@@ -4,7 +4,12 @@ const sequelize = require("../config/database");
 const Warehouse = sequelize.define(
   "Warehouse",
   {
-    name: {
+    id: {
+      type: DataTypes.BIGINT,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    location: {
       type: DataTypes.STRING,
       allowNull: false,
     },
