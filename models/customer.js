c4ff10d6dde0453da409585090e6c60fd8753d10
ＫＥＮＴ <kent@Kent-Mac-mini.local>
2025-02-18 17:00:45 +0800

    2025-02-18

--- models/customer.js
+++ models/customer.js
@@ -10,15 +10,15 @@ const Customer = sequelize.define(
       primaryKey: true,
     },
     name: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING(255),
       allowNull: false,
     },
     email: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING(320),
       unique: true,
     },
     phone_number: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING(15),
       unique: true,
     },
   },
