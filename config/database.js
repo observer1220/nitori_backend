ae227fedf3e8c7df7d7b1ee70b53b2e00b6264f6
ＫＥＮＴ <kent@Kent-Mac-mini.local>
2025-03-25 19:41:18 +0800

    2025-03-25

--- config/database.js
+++ config/database.js
@@ -2,7 +2,21 @@
 const { Sequelize } = require("sequelize");
 
 // 使用 Docker Volume
-const sequelize = new Sequelize(process.env.DB_SOURCE);
+const sequelize = new Sequelize(process.env.DB_SOURCE, {
+  dialect: "postgres",
+  logging: (msg, time) => {
+    // 只記錄超過 1 秒的查詢
+    if (time > 1000) {
+      console.log(`[Sequelize] Slow Query (${time}ms): ${msg}`);
+    }
+  },
+  pool: {
+    max: 5, // 最大連線數
+    min: 1, // 最小連線數
+    acquire: 30000, // 30 秒內未獲得連線則報錯
+    idle: 10000, // 10 秒內無使用則釋放
+  },
+});
 
 // 使用本地端資料庫
 // const sequelize = new Sequelize("nitori", "root", "test1234", {
